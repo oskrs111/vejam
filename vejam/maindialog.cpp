969733b35f9d9107112ac31b9e3c807479a761a5
oskrs111 <oskrs111@gmail.com>
2014-08-29 10:14:05 +0200

    Some improvements: 1-Missing .exe icon fix.

---
+++ vejam/maindialog.cpp
@@ -0,0 +1,24 @@
+#include "maindialog.h"
+#include "ui_maindialog.h"
+
+mainDialog::mainDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::mainDialog)
+{
+    ui->setupUi(this);
+}
+
+mainDialog::~mainDialog()
+{
+    delete ui;
+}
+
+void mainDialog::on_okButton_clicked()
+{
+
+}
+
+void mainDialog::on_configButton_clicked()
+{
+
+}
